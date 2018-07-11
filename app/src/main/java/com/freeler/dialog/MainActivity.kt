659f6548acfe5b8f479fddb5c80ff8276334f6cb
472184081@qq.com <472184081@qq.com>
2018-07-11 11:46:30 +0800

    扩展代码说明

--- app/src/main/java/com/freeler/dialog/MainActivity.kt
+++ app/src/main/java/com/freeler/dialog/MainActivity.kt
@@ -2,17 +2,21 @@ package com.freeler.dialog
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.Toast
 import com.freeler.iosdialog.IOSDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var dialog: IOSDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mBtn.setOnClickListener {
-            IOSDialog.Builder(this)
+            dialog = IOSDialog.Builder(this)
                     .setTitle("我是Dialog")
                     .setSubTitle("IOS")
                     .setNegativeText("取消")
@@ -21,6 +25,20 @@ class MainActivity : AppCompatActivity() {
                     .setOnPositiveClickListener { v, dialog -> dialog?.dismiss() }
                     .setCancelable(false)
                     .show()
+
+            //获取对象
+            val alertDialog = dialog?.alertDialog
+            val window = alertDialog?.window
+
+            //设置自定义childView相关
+            val titleTextView = dialog?.getView<TextView>(R.id.tv_title)
+            dialog?.setChildViewText(R.id.tv_title, "这是标题")
+            dialog?.setChildViewColor(R.id.tv_title, R.color.colorAccent)
+            dialog?.setChildViewSize(R.id.tv_title, 14)
+            dialog?.setChildViewClick(R.id.tv_title, {
+                Toast.makeText(this@MainActivity, "点击标题", Toast.LENGTH_SHORT).show()
+            })
+
         }
     }
 }
